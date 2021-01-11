9e62c76d8f0a41a52eac8087cd682eef802e85f2
Fernando <fernandoguillen.apps@gmail.com>
2021-01-11 12:32:23 -0600

    add git ignore

--- app.js
+++ app.js
@@ -187,6 +187,11 @@ app.get('/logout', function(req,res){
     res.redirect('/');
 })
 
-app.listen(3000, function(){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
    console.log('Server Started!');
 });
